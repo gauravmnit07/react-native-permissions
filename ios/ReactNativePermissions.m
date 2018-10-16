79ae2ea64f8eb8288a7d96e4063aac47a33a8dd4
Gaurav Bansal <gaurav.bansal@sprinklr.com>
2018-10-16 21:45:34 +0530

    ios: Removing unused permissions

--- ios/ReactNativePermissions.m
+++ ios/ReactNativePermissions.m
@@ -34,17 +34,11 @@
   #import "RCTEventDispatcher.h"
 #endif
 
-#import "RNPLocation.h"
-#import "RNPBluetooth.h"
 #import "RNPNotification.h"
 #import "RNPAudioVideo.h"
-#import "RNPEvent.h"
 #import "RNPPhoto.h"
-#import "RNPContacts.h"
 #import "RNPBackgroundRefresh.h"
-#import "RNPSpeechRecognition.h"
 #import "RNPMediaLibrary.h"
-#import "RNPMotion.h"
 
 
 @interface ReactNativePermissions()
